3aeca19a4a0056f6eecd147b536d71809d70659b
Bohdan Butsev <44702726+winnerok7@users.noreply.github.com>
2022-02-14 04:49:21 +0200

    Add files via upload
    day 2

---
+++ intense/js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.main-slider', {
+   slidesPerView: 2,
+   spaceBetween: 30,
+   loop: true,
+
+   // Navigation arrows
+   navigation: {
+      nextEl: '.main-slider-arrow',
+   },
+
+});
